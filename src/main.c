08108f22eb02dd797fcc80a6686872c017307a0e
DaelonSuzuka <david@ldgelectronics.com>
2020-12-06 21:54:37 -0500

    Fixed shell setup/teardown code

--- src/main.c
+++ src/main.c
@@ -1,35 +1,29 @@
+#include "shell/shell.h"
 #include <stdio.h>
 #include <termios.h>
 #include <unistd.h>
-#include "shell/shell.h"
 
 static struct termios old, new1;
-void init_termios(int echo)
-{
-    tcgetattr(0, &old);                  /* grab old terminal i/o settings */
-    new1 = old;                          /* make new settings same as old settings */
-    new1.c_lflag &= ~ICANON;             /* disable buffered i/o */
+void init_termios(int echo) {
+    tcgetattr(0, &old);      /* grab old terminal i/o settings */
+    new1 = old;              /* make new settings same as old settings */
+    new1.c_lflag &= ~ICANON; /* disable buffered i/o */
     new1.c_lflag &= echo ? ECHO : ~ECHO; /* set echo mode */
-    tcsetattr(0, TCSANOW, &new1);        /* use these new terminal i/o settings now */
+    tcsetattr(0, TCSANOW, &new1); /* use these new terminal i/o settings now */
 }
 
-void reset_termios(void)
-{
-    tcsetattr(0, TCSANOW, &old);
-}
+void reset_termios(void) { tcsetattr(0, TCSANOW, &old); }
 
-int main(void)
-{
-    printf("Hello World\n");
+int main(void) {
     init_termios(0);
 
-    // shell_t shell;
-    // shell_init(shell);
+    shell_init();
 
-    // while(1) {
-    //     char currentChar = getchar();
-    //     shell_update(shell, currentChar);
-    // }
+    while (1) {
+        // char currentChar = getchar();
+        // shell_update(currentChar);
+        shell_update();
+    }
 
     reset_termios();
     return 0;
